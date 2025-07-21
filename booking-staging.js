d9934c62d58df6fee3bf64f00bcd700f350b9e15
Uditha Milanka Pathirana <119309622+uditha-milanka@users.noreply.github.com>
2025-07-21 11:30:04 +0530

    Create booking-staging.js

---
+++ booking-staging.js
@@ -0,0 +1,20 @@
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    const iframe = document.getElementById('public-booking-iframe');
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const paramValue = urlParams.get('token');
+
+    if (paramValue && iframe) {
+      iframe.setAttribute('src', `https://staging-buddy-frontend-zeta.vercel.app/bookings/manage/authorisation/${paramValue}`);
+    }
+
+    window.addEventListener('message', function (event) {
+      if (event.data && event.data.type === 'customRedirect') {
+        const finalUrl = event.data.data.redirectUrl;
+        window.location.href = `${finalUrl}&public=public_url&currentUrl=${window.location.href}`;
+      }
+    });
+  });
+})();
