3767eaa8436811f275381c3293c088f247bbb83d
Uditha Milanka Pathirana <119309622+uditha-milanka@users.noreply.github.com>
2025-07-23 08:52:34 +0530

    Create booking-test3.js

---
+++ booking-test3.js
@@ -0,0 +1,29 @@
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    const iframe = document.getElementById('public-booking-iframe');
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const paramValue = urlParams.get('token');
+    const redirectParamValue = urlParams.get('redirectToken');
+
+    if ('scrollRestoration' in history) {
+      history.scrollRestoration = 'manual';
+    }
+
+    if (paramValue && iframe) {
+      iframe.setAttribute('src', `https://staging-buddy-frontend-zeta.vercel.app/bookings/manage/authorisation/${paramValue}`);
+    }
+
+    if (redirectParamValue && iframe) {
+      iframe.setAttribute('src', `https://staging-buddy-frontend-zeta.vercel.app/bookings/manage/summary/${redirectParamValue}`);
+    }
+
+    window.addEventListener('message', function (event) {
+      if (event.data && event.data.type === 'customRedirect') {
+        const finalUrl = event.data.data.redirectUrl;
+        window.location.href = `${finalUrl}&public=public_url&currentUrl=${window.location.href}`;
+      }
+    });
+  });
+})();
