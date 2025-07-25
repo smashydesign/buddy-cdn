1e4fbb1141ffe185c5d86489b66cdf101e890ca4
Uditha Milanka Pathirana <119309622+uditha-milanka@users.noreply.github.com>
2025-07-25 15:34:33 +0530

    Create buddy-production.js

---
+++ buddy-production.js
@@ -0,0 +1,25 @@
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    const iframe = document.getElementById('public-booking-iframe');
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const paramValue = urlParams.get('token');
+    const redirectParamValue = urlParams.get('redirectToken');
+
+    if (paramValue && iframe) {
+      iframe.setAttribute('src', `https://app.buddy.rip/bookings/manage/authorisation/${paramValue}`);
+    }
+
+    if (redirectParamValue && iframe) {
+      iframe.setAttribute('src', `https://app.buddy.rip/bookings/manage/summary/${redirectParamValue}`);
+    }
+
+    window.addEventListener('message', function (event) {
+      if (event.data && event.data.type === 'customRedirect') {
+        const finalUrl = event.data.data.redirectUrl;
+        window.location.href = `${finalUrl}&public=public_url&currentUrl=${window.location.href}`;
+      }
+    });
+  });
+})();
